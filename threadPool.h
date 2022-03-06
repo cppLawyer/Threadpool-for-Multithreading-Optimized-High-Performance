c9a1ad83e3fad96a6024987ff4c4c0794efc8c29
cppLawyer <76749832+cppLawyer@users.noreply.github.com>
2022-03-06 18:55:35 +0100

    Add files via upload

--- threadPool.h
+++ threadPool.h
@@ -16,44 +16,79 @@ private:
 	std::atomic<bool> stop;
  public:
 	 threadpool() {
-		 stop.store(false,std::memory_order_acq_rel);
+		 stop.store(false,std::memory_order_release);
 		 pool.reserve(this->total_threads);
 		 pool.push_back(std::move(std::thread([&]() {
 
 			 do {
-				 if (!this->task_queue.empty() && this->pool.size() != this->total_threads) {
-					 this->pool.push_back(std::move(std::thread(std::move(this->task_queue.front()))));
-					 this->task_queue.pop();
-				 }
-				 else {
-					 if (!this->pool[1].joinable()) {
-						 this->pool.erase(this->pool.begin() + 1, this->pool.begin() + 2);
-					 }
-					 else {
-						 this->pool[1].join();
-					 }
+				 while(!(this->stop.load(std::memory_order_consume)) && this->task_queue.empty() && this->pool.size() == 1) {
 					 std::this_thread::yield();
 				 }
-
-			 } while (!(this->stop.load(std::memory_order_consume)) || this->task_queue.size() != 0 || this->pool.size() != 1); })));
+				 
+					 if (!this->task_queue.empty() && this->pool.size() != this->total_threads) {
+						 this->pool.push_back(std::move(std::thread(std::move(this->task_queue.front()))));
+						 this->task_queue.pop();
+					 }
+					 else {
+						 if (this->pool[1].joinable()) {
+							 this->pool[1].join();
+							 this->pool.erase(this->pool.begin() + 1, this->pool.begin() + 2);
+						 }
+					 }
+				 
+				 
+			 }while (!(this->stop.load(std::memory_order_consume)) || !this->task_queue.empty() || this->pool.size() != 1); })));
 	 }
 	 inline void push(std::function<void()>&& f1) {
 		 task_queue.push(std::move(f1));
 	 }
 	 inline void stop_pool() {
 		 //manually stop the threadpool
-		 this->stop.store(true,std::memory_order_acq_rel);
+		 this->stop.store(true,std::memory_order_release);
+		 if (pool[0].joinable()) {
+			 pool[0].join();
+		 }
+	 }
+	 inline void reset_and_restart() {
+		 assert(!(pool[0].joinable())); //"Call stop_pool() before the reset_and_restart()"
+		 assert(!(task_queue.size() > 0));
+		 pool.clear();
+		 stop.store(false, std::memory_order_release);
+		 pool.push_back(std::move(std::thread([&]() {
+
+			 do {
+				 while (!(this->stop.load(std::memory_order_consume)) && this->task_queue.empty() && this->pool.size() == 1) {
+					 std::this_thread::yield();
+				 }
+				 if (!this->task_queue.empty() && this->pool.size() != this->total_threads) {
+					 this->pool.push_back(std::move(std::thread(std::move(this->task_queue.front()))));
+					 this->task_queue.pop();
+				 }
+				 else {
+					 if (this->pool[1].joinable()) {
+						 this->pool[1].join();
+						 this->pool.erase(this->pool.begin() + 1, this->pool.begin() + 2);
+					 }
+				 }
+				
+			 } while (!(this->stop.load(std::memory_order_consume)) || !this->task_queue.empty() || this->pool.size() != 1); })));
+
+
 	 }
 	 ~threadpool() {
-		 if (!stop.load(std::memory_order_acquire)) {
-			 stop.store(true,std::memory_order_acq_rel);
+		 if (!stop.load(std::memory_order_consume)) {
+			 stop.store(true, std::memory_order_relaxed);
 			 //automatic stop
 		 }
-		 pool[0].join();
+		 if (pool[0].joinable()) {
+			 pool[0].join();
+		 }
+
 	 }
 	
 };
 
 #endif
+
 
 	
